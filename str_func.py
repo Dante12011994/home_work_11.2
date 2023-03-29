a21a039131146d243f70144a6432e6874be605d7
Alexander <dante1201@mail.ru>
2023-03-29 18:37:47 +0300

    add: def capitalize and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def upper(text):
+    """
+    Весь текст в верхнем регистре
+    """
     return text.upper()
+
+
+def capitalize(text):
+    """
+    Каждое слово с заглавной буквы
+    """
+    return text.capitalize()
