8aef8ffa01e2cd22ca3b3d418f9d2a705240de96
Dante <dante1201@mail.ru>
2023-04-01 10:54:15 +0300

    ййй

--- str_func.py
+++ str_func.py
@@ -10,3 +10,12 @@ def capitalize(text):
     Каждое слово с заглавной буквы
     """
     return text.capitalize()
+
+def lower(text):
+    """
+    Возвращает все маленькими буквами
+
+    :param text:
+    :return:
+    """
+    return text.lower()
